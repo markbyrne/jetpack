f152e7bbf8957438d823f4b78ab5a0691b71ed24
Allison Levine <allison.levine@automattic.com>
2021-10-19 11:42:51 -0400

    [not verified] Fix rebase, back out connections updated state, get new object ref for connection

--- projects/plugins/jetpack/extensions/plugins/publicize/components/form/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/form/index.js
@@ -7,11 +7,6 @@
  */
 
 /**
- * External dependencies
- */
-import { useEffect, useState } from 'react';
-
-/**
  * WordPress dependencies
  */
 import { PanelRow } from '@wordpress/components';
@@ -26,23 +21,9 @@ import MessageBoxControl from '../message-box-control';
 import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 import useSocialMediaMessage from '../../hooks/use-social-media-message';
 
-<<<<<<< HEAD
 export default function PublicizeForm( { isPublicizeEnabled, isRePublicizeFeatureEnabled } ) {
 	const { connections, toggleById, hasConnections } = useSocialMediaConnections();
-=======
-export default function PublicizeForm() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	const { connections, toggleById, refresh } = useSocialMediaConnections();
->>>>>>> d28ddc734c ([not verified] Refresh connections on change)
-=======
-	const { connections, toggleById } = useSocialMediaConnections();
->>>>>>> eca1ee6b47 ([not verified] Trigger state change when connections change)
-=======
-	const { connections, connectionsUpdated, toggleById } = useSocialMediaConnections();
->>>>>>> f0bda6823e (Add connectionsUpdated to state to signal change)
 	const { message, updateMessage, maxLength } = useSocialMediaMessage();
-	const [ activeConnections, setActiveConnections ] = useState( {} );
 
 	function isDisabled() {
 		// Do not disable when RePublicize is enabled.
@@ -53,13 +34,8 @@ export default function PublicizeForm() {
 		return connections.every( connection => ! connection.toggleable );
 	}
 
-	useEffect( () => {
-		setActiveConnections( connections );
-	}, [ connectionsUpdated, connections ] );
-
 	return (
 		<Fragment>
-<<<<<<< HEAD
 			{ hasConnections && (
 				<PanelRow>
 					<ul className="jetpack-publicize__connections-list">
@@ -67,15 +43,6 @@ export default function PublicizeForm() {
 							( { display_name, enabled, id, service_name, toggleable, profile_picture } ) => (
 								<PublicizeConnection
 									disabled={ isRePublicizeFeatureEnabled ? ! isPublicizeEnabled : ! toggleable }
-=======
-			<PanelRow>
-				<ul className="jetpack-publicize__connections-list">
-					{ activeConnections.length > 0 &&
-						activeConnections.map(
-							( { display_name, enabled, id, service_name, toggleable, profile_picture } ) => (
-								<PublicizeConnection
-									disabled={ ! toggleable }
->>>>>>> eca1ee6b47 ([not verified] Trigger state change when connections change)
 									enabled={ enabled }
 									key={ id }
 									id={ id }
@@ -86,14 +53,9 @@ export default function PublicizeForm() {
 								/>
 							)
 						) }
-<<<<<<< HEAD
 					</ul>
 				</PanelRow>
 			) }
-=======
-				</ul>
-			</PanelRow>
->>>>>>> eca1ee6b47 ([not verified] Trigger state change when connections change)
 
 			<PublicizeSettingsButton />
 
